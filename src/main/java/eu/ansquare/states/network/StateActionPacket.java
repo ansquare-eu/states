42b5230f1edb7ef616f856dad0331b3c6f98807c
farpo <77583105+farpo@users.noreply.github.com>
2023-11-23 07:12:36 +0100

    Wow i made it work

--- src/main/java/eu/ansquare/states/network/StateActionPacket.java
+++ src/main/java/eu/ansquare/states/network/StateActionPacket.java
@@ -16,16 +16,19 @@ import java.util.logging.Logger;
 
 public class StateActionPacket {
 	public static void receive(MinecraftServer server, ServerPlayerEntity player, ServerPlayNetworkHandler handler, PacketByteBuf buf, PacketSender responseSender){
-			int[] ints = buf.readIntArray();
+		int[] ints = buf.readIntArray();
+		server.execute(() -> {
 			BlockPos pos = new BlockPos(ints[0], ints[1], ints[2]);
 			BlockEntity entity = player.getServerWorld().getBlockEntity(pos);
+			States.LOGGER.info(pos.toString());
 			if(entity instanceof StateBlockEntity stateBlock) {
 				stateBlock.loadFromStack(ints[3]);
 				//BlockEntity entity = player.getServerWorld().getBlockEntity(buf.readBlockPos());
 				//if(entity instanceof StateBlockEntity stateBlock){
 				//	stateBlock.loadFromStack(buf.readInt());
 				//}
-			}
+			}});
+
 	}
 }
 
