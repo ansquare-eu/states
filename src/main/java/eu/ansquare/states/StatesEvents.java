6c1b7c6537dbda73cd0aa83e66f34574b9a59459
farpo <77583105+farpo@users.noreply.github.com>
2023-11-26 10:52:11 +0100

    messages

--- src/main/java/eu/ansquare/states/StatesEvents.java
+++ src/main/java/eu/ansquare/states/StatesEvents.java
@@ -6,6 +6,7 @@ import eu.ansquare.states.block.StatesBlocks;
 import net.fabricmc.fabric.api.event.player.AttackBlockCallback;
 import net.fabricmc.fabric.api.event.player.AttackEntityCallback;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.minecraft.item.BlockItem;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 
@@ -23,6 +24,7 @@ public class StatesEvents {
 		AttackBlockCallback.EVENT.register((player, world, hand, pos, direction) -> {
 			if(!world.isClient()){
 				if(!StatePermission.permissionAt(world.getChunk(pos), player).maybuild){
+					player.sendMessage(Text.translatable("state.deny.break"), true);
 					return ActionResult.FAIL;
 				}
 			}
@@ -31,8 +33,16 @@ public class StatesEvents {
 		UseBlockCallback.EVENT.register((player, world, hand, hitResult) -> {
 			if(!world.isClient()){
 				if(!StatePermission.permissionAt(world.getChunk(hitResult.getBlockPos().offset(hitResult.getSide())), player).maybuild){
+					if(player.getStackInHand(hand).getItem() instanceof BlockItem && !player.isSneaking()){
+						player.sendMessage(Text.translatable("state.deny.build"), true);
+					}else {
+						player.sendMessage(Text.translatable("state.deny.interact"), true);
+					}
+
 					return ActionResult.FAIL;
 				} else if (StatePermission.isClaimed(world, hitResult.getBlockPos().offset(hitResult.getSide())) && player.getStackInHand(hand).isIn(States.STATEMAKERS)) {
+					player.sendMessage(Text.translatable("state.deny.state"), true);
+
 					return ActionResult.FAIL;
 				}
 			}
